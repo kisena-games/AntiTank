03060351485929848d48784ccb33a8d63586cca1
Anton Ferzov <magicend911@outlook.com>
2025-01-11 20:47:25 +0700

    added state machine and leanpool

---
+++ Assets/Anton_Developer/Scripts/Enemy/MoveState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MoveState : State
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, _speed * Time.deltaTime);
+    }
+}
