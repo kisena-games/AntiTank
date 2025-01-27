607572c5c051ac34abd2d7218a53ed9824aa31f0
Anton Ferzov <magicend911@outlook.com>
2025-01-27 22:32:00 +0700

    added pause class

---
+++ Assets/Anton_Developer/Scripts/SetPause.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public bool IsPause { get; private set; }
+
+    public static GamePause Instance;
+
+    private List<IsPauseHandler> _pauses = new List<IsPauseHandler>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance == this)
+            Instance = null;
+    }
+
+    public void SetPause(bool isEnable)
+    {
+        IsPause = isEnable;
+
+        for (int i = 0; i < _pauses.Count; i++)
+        {
+            _pauses[i].IsPaused(isEnable);
+        }
+    }
+
+    public void AddPauseList(IsPauseHandler pauseHandler)
+    {
+        _pauses.Add(pauseHandler);
+    }
+
+    public void RemovePauseList(IsPauseHandler pauseHandler)
+    {
+        _pauses.Remove(pauseHandler);
+    }
+}
+
+public interface IsPauseHandler
+{
+    void IsPaused(bool isPaused);
+}
