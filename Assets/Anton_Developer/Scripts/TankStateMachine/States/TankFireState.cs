2daa25636df3e9a85dccc24ed279d9ff964dfa00
kisena-games <kisena.games@gmail.com>
2025-01-28 19:29:51 +0300

    Realized state machine AI move and for tanks Corrected tanks models and animators

--- Assets/Anton_Developer/Scripts/TankStateMachine/States/TankFireState.cs
+++ Assets/Anton_Developer/Scripts/TankStateMachine/States/TankFireState.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TankFireState : State
 {
-    private TankAnimationController _controller;
+    private readonly TankAnimationController _controller;
+    private readonly NavMeshAgent _agent;
+    private readonly Transform _aimToAttack;
+    private readonly Transform _tank;
+    private readonly GameObject _bulettPrefab;
 
-    public TankFireState(TankAnimationController controller)
+    Vector3 _targetDirection = Vector3.zero;
+
+    public TankFireState(TankAnimationController controller, NavMeshAgent agent, Transform tank, Transform aimToAttack, GameObject bulettPrefab)
     {
         _controller = controller;
+        _agent = agent;
+        _aimToAttack = aimToAttack;
+        _tank = tank;
+        _bulettPrefab = bulettPrefab;
+        _targetDirection = (_aimToAttack.position - _tank.position);
     }
 
     public override void OnEnter()
     {
+        _agent.updateRotation = false;
+        _tank.rotation = Quaternion.FromToRotation(_tank.forward, -_targetDirection);
+
         _controller.SetBool(TankAnimationType.FireBool, true);
     }
 
     public override void OnExit()
     {
         _controller.SetBool(TankAnimationType.FireBool, false);
+    }
+
+    public override void OnUpdate()
+    {
+        
     }
 }
