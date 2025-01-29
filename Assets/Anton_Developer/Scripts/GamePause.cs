67314dc9d29c2999f9074c4b2e95444e207ce6cf
Anton Ferzov <magicend911@outlook.com>
2025-01-29 22:46:54 +0700

    some changes for pause

--- Assets/Anton_Developer/Scripts/GamePause.cs
+++ Assets/Anton_Developer/Scripts/GamePause.cs
@@ -1,18 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : MonoBehaviour
+public class GamePause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool IsPause { get; private set; }
+
+    public static GamePause Instance;
+
+    private List<IsPauseHandler> _pauses = new List<IsPauseHandler>();
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if(Instance == this)
+            Instance = null;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetPause(!IsPause);
+        }
+    }
+
+    public void SetPause(bool isEnable)
+    {
+        IsPause = isEnable;
+
+        for (int i = 0; i < _pauses.Count; i++)
+        {
+            _pauses[i].IsPaused(isEnable);
+        }
+    }
+
+    public void AddPauseList(IsPauseHandler pauseHandler)
+    {
+        if (!_pauses.Contains(pauseHandler))
+        {
+            _pauses.Add(pauseHandler);
+        }
+    }
+
+    public void RemovePauseList(IsPauseHandler pauseHandler)
+    {
+        _pauses.Remove(pauseHandler);
+    }
+}
+
+public interface IsPauseHandler
+{
+    void IsPaused(bool isPaused);
 }
