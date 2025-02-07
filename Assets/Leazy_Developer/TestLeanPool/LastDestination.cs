2dc5906319f2270d007bba3cd5f639ec1fbb1ccb
kisena-games <kisena.games@gmail.com>
2025-02-07 19:46:52 +0300

    Fixed LastDestination script

--- Assets/Leazy_Developer/TestLeanPool/LastDestination.cs
+++ Assets/Leazy_Developer/TestLeanPool/LastDestination.cs
@@ -5,8 +5,26 @@ using UnityEngine;
 public class LastDestination : MonoBehaviour
 {
     private Transform _tank;
+    private HealthManager _tankHealth;
 
-    public bool IsFree => _tank == null;
+    public bool IsFree
+    {
+        get
+        {
+            if (_tank != null && _tankHealth.IsKilled)
+            {
+                _tank = null;
+                _tankHealth = null;
+            }
+
+            if (_tank == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
 
     private void OnDrawGizmos()
     {
@@ -14,11 +32,17 @@ public class LastDestination : MonoBehaviour
         Gizmos.DrawSphere(transform.position, 2f);
     }
 
+    private void Update()
+    {
+        Debug.Log(IsFree);
+    }
+
     public bool TryPlaceTank(Transform tank)
     {
         if (IsFree)
         {
             _tank = tank;
+            _tankHealth = tank.GetComponent<HealthManager>();
             return true;
         }
 
