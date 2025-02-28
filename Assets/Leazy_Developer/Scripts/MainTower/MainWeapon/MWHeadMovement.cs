5f0ef147089581de47c2763fc7640e8d7de38778
Alexeiad <103834584+Alexeiad@users.noreply.github.com>
2025-02-28 18:28:07 +0300

    - possible change of bug

--- Assets/Leazy_Developer/Scripts/MainTower/MainWeapon/MWHeadMovement.cs
+++ Assets/Leazy_Developer/Scripts/MainTower/MainWeapon/MWHeadMovement.cs
@@ -9,7 +9,7 @@ public class MWHeadMovement : MonoBehaviour
 {
     [Header("Sniper Mode Parameters")]
     [SerializeField] private float _mouseSensitivity = 2.0f;
-    [SerializeField] private float verticalClampAngle = 10.0f;
+    [SerializeField] private float verticalClampAngle = 30.0f;
     [SerializeField] private float horizontalClampAngle = 100.0f;
 
     private Vector3 _directionCameraToWeapon = Vector3.zero;
